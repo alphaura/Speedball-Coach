26c60f72a7ebc6d55bf22a093ccf9bdbab0b45d7
alphaura <kylemagri98@gmail.com>
2024-01-22 19:31:16 +0100

    Added APK folder

--- index.js
+++ index.js
@@ -1,26 +1,4 @@
 
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js"
-
-// import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js"
-
-
-// // Your web app's Firebase configuration
-// const firebaseConfig = {
-// apiKey: "AIzaSyA-M7FA8OqeXe3K5NxcrxVhuu2A_jZU3S4",
-// authDomain: "speedball-coach.firebaseapp.com",
-// projectId: "speedball-coach",
-// storageBucket: "speedball-coach.appspot.com",
-// messagingSenderId: "189427787225",
-// appId: "1:189427787225:web:ba072e0269249f354365c8"
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const database = getDatabase(app)
-// const selfReview = ref(database, "selfReview")
-
-
 // Filter section
 
 const allSessionsFilter = document.getElementById("all-sessions")
